0717f44ab0c9e5a4d4cee66cff180573c228ae64
Max <maximilian@dahos.de>
2022-01-08 17:25:41 +0100

    Initial Commit

---
+++ TestGIT/TestGITApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGITApp.swift
+//  TestGIT
+//
+//  Created by Maximilian Schröder on 08.01.22.
+//
+
+import SwiftUI
+
+@main
+struct TestGITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
